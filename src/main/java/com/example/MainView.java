4001b9314a98d8d99594e0955e8f07d36d0e278f
liviawelschinger <livia-welschinger@t-online.de>
2020-03-31 11:34:53 +0200

    Initial commit

---
+++ src/main/java/com/example/MainView.java
@@ -0,0 +1,65 @@
+package com.example;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * A sample Vaadin view class.
+ * <p>
+ * To implement a Vaadin view just extend any Vaadin component and
+ * use @Route annotation to announce it in a URL as a Spring managed
+ * bean.
+ * Use the @PWA annotation make the application installable on phones,
+ * tablets and some desktop browsers.
+ * <p>
+ * A new instance of this class is created for every new user and every
+ * browser tab/window.
+ */
+@Route
+@PWA(name = "Vaadin Application",
+        shortName = "Vaadin App",
+        description = "This is an example Vaadin application.",
+        enableInstallPrompt = true)
+@CssImport("./styles/shared-styles.css")
+@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+public class MainView extends VerticalLayout {
+
+    /**
+     * Construct a new Vaadin view.
+     * <p>
+     * Build the initial UI state for the user accessing the application.
+     *
+     * @param service The message service. Automatically injected Spring managed bean.
+     */
+    public MainView(@Autowired GreetService service) {
+
+        VerticalLayout todosList = new VerticalLayout(); // vertical layout that displays the task and checkboxes
+        TextField taskfield = new TextField(); // text input field for new tasks
+        Button addButton = new Button("Add"); // triggers logic to add new todo items
+        addButton.addClickShortcut(Key.ENTER);
+       addButton.addClickListener(click -> {
+           Checkbox checkbox = new Checkbox(taskfield.getValue());
+           todosList.add(checkbox);
+        });
+
+        // call add on VerticalLayout to define how the components should be displayed
+        add(
+                new H1("Vaadin Todo List"),
+                todosList,
+                new HorizontalLayout(
+                        taskfield,
+                        addButton
+                )
+        );
+    }
+
+}
